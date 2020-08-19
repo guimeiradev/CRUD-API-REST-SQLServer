97bc276b856186c3645e091342340b099ff4da92
guimeiradev <gmeira2006@gmail.com>
2020-08-19 13:51:48 -0300

    CRUDZIN

---
+++ WebApplicationAluno/Models/Aluno.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplicationAluno.Models {
+    public class Aluno {
+        public int AlunoId { get; set; }
+        public string Nome { get; set; }
+        public string Email { get; set; }
+
+    }
+}
+
